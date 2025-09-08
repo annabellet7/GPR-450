d881a92e7c671a7decd251d37661678f8f08592f
Olivia <omsanta123@gmail.com>
2025-09-08 11:44:58 -0400

    adding pseudo-code from class
    :D

--- animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_src/a3_KeyframeAnimationController.c
+++ animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_src/a3_KeyframeAnimationController.c
@@ -54,8 +54,44 @@ a3i32 a3clipControllerUpdate(a3_ClipController* clipCtrl, a3f64 dt)
 //-----------------------------------------------------------------------------
 //****TO-DO-ANIM-PROJECT-1: IMPLEMENT ME
 //-----------------------------------------------------------------------------
-
-
+		//1. pre-resolution: apply time step: single line of code applied twice to increment "keyframe time" and "clip time" by the time step
+//	a. time step: take in only a positive time step (1/30 of dt)
+//	b. numberical integration for time itself
+// 
+//2. resolve time: while unresolved, continue playback behavior to determine new "keyframe time" and "clip time"
+//	a. 7 cases to check for resolution total
+//		i. resolve keyframe
+//			1. paused: dt = 0
+//			2. forward: dt > 0
+//				a. stop
+//				b. step(s) taken
+//				c. clip exited
+//			3. reverse: dt < 0
+//				a. stop
+//				b. step(s) taken
+//				c. clip exited
+//			4. loop
+//				a. calculate overstep t-t1
+//				b. t -> overstep + t0 
+//				c. direction = direction
+//			5. ping-pong
+//				a. t = t1 - overstep
+//				b. 1 -> 3
+//				c. direction = -direction
+//			6. (go to and) stop
+//				a. t = duration of clip
+//				b. i = ??? (i = 3 or 4, problems either way)
+//				c. direction = 0
+//			7. it says 7 but I only counted 6 :(
+// 
+//3. post-resolution: normalize time/parameters: one time, single line of code applied twice to calculate normalized "keyframe time" and "clip time"
+//	a. normalize keyframe/clip time: relative time / duration
+//	b. 0-1 (not clamped)
+// 
+//4. add a new instance of the clip controller, new data, and link them to something in the scene to produce a new animation effect
+//	a. required for team of 3
+// 
+//BONUS. Parse the custom clip data file format provided to automate the preparation of clips and keyframe data
 
 //-----------------------------------------------------------------------------
 //****END-TO-DO-PROJECT-1
