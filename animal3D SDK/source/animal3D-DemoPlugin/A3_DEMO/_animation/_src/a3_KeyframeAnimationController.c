b8d383ad59c1193852b281a1a8380cf685891249
annabellet-7 <athompson@winslowtg.com>
2025-09-12 06:27:39 -0400

    comments

--- animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_src/a3_KeyframeAnimationController.c
+++ animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_src/a3_KeyframeAnimationController.c
@@ -22,6 +22,24 @@
 	Implementation of keyframe animation controller.
 */
 
+/*
+	Short description of code goes here.
+
+	Author: Victor Diab, Olivia Santamauro, Annabelle Thompson
+	Class: GPR-450 Advanced Animation Programming
+	Assignment: Interactive Keyframe & Time Control
+
+	Certification of Authenticity:
+	I certify that this is entirely my own work, except where I have given
+	fully-documented references to the work of others. I understand the definition
+	and consequences of plagiarism and acknowledge that the assessor of this
+	assignment may, for the purpose of assessing this assignment:
+	- Reproduce this assignment and provide a copy to another member of academic
+	- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+	- service (which may then retain a copy of this assignment on its database for
+	- the purpose of future plagiarism checking)
+*/
+
 #include "../a3_KeyframeAnimationController.h"
 
 #include <string.h>
@@ -49,6 +67,9 @@ a3i32 a3clipControllerInit(a3_ClipController* clipCtrl_out, const a3byte ctrlNam
 
 
 // update clip controller
+// Authors: Victor, Olivia, Annabelle
+//	-Code blocks do not have individual authors as most code was 
+//	-written while we were working together in a group.
 a3i32 a3clipControllerUpdate(a3_ClipController* clipCtrl, a3f64 dt)
 {
 	if (clipCtrl && clipCtrl->clipPool)
@@ -56,15 +77,7 @@ a3i32 a3clipControllerUpdate(a3_ClipController* clipCtrl, a3f64 dt)
 //-----------------------------------------------------------------------------
 //****TO-DO-ANIM-PROJECT-1: IMPLEMENT ME
 //-----------------------------------------------------------------------------
-// 
-//-----------------------------------------------------------------------------
-// 		   Olivia Notes
-//-----------------------------------------------------------------------------
-// 
-		//1. pre-resolution: apply time step: single line of code applied twice to increment "keyframe time" and "clip time" by the time step
-//	a. time step: take in only a positive time step (1/30 of dt)
-//	b. numberical integration for time itself
-// 
+
 		a3f64 timeStep;
 		switch (clipCtrl->state)
 		{
@@ -82,47 +95,14 @@ a3i32 a3clipControllerUpdate(a3_ClipController* clipCtrl, a3f64 dt)
 		}
 		clipCtrl->clipTime_sec += timeStep;
 		clipCtrl->keyframeTime_sec += timeStep;
-// 
-//2. resolve time: while unresolved, continue playback behavior to determine new "keyframe time" and "clip time"
-//	a. 7 cases to check for resolution total
-//		i. resolve keyframe
-//			1. paused: dt = 0
-//			2. forward: dt > 0
-//				a. stop
-//				b. step(s) taken
-//				b. step(s) taken
-//				c. clip exited
-//			3. reverse: dt < 0
-//				a. stop
-//				b. step(s) taken
-//				c. clip exited
-//			4. loop
-//				a. calculate overstep t-t1
-//				b. t -> overstep + t0 
-//				c. direction = direction
-//			5. ping-pong
-//				a. t = t1 - overstep
-//				b. 1 -> 3
-//				c. direction = -direction
-//			6. (go to and) stop
-//				a. t = duration of clip
-//				b. i = ??? (i = 3 or 4, problems either way)
-//				c. direction = 0
-//			7. it says 7 but I only counted 6 :(
 		
 		//if clip time exits clip bounds
 		while (/*time is unresolved*/clipCtrl->clipTime_sec > clipCtrl->clip->duration_sec || clipCtrl->clipTime_sec < 0)
 		{
-			if (timeStep > 0)
-			{
-				//time is greater than or equal to the clip duration
-				
-				//looping
-				// calculate overstep (how far into the clip/keyframe time should be after loop)
-				//		?: offset = clipCtrl->clipTime_sec - clipCtrl->clip->duration_sec
-				//set keyframe index to 0
-				//add offset
-				//check if in correct keyframe
+			//time is greater than or equal to the clip duration (forward)
+			if (timeStep >= 0)
+			{	
+				//loops back to first key frame after last one
 				if (clipCtrl->clip->transitionForward->flag == a3clip_playFlag)
 				{
 					a3f64 overstep = clipCtrl->clipTime_sec - clipCtrl->clip->duration_sec;
@@ -131,6 +111,7 @@ a3i32 a3clipControllerUpdate(a3_ClipController* clipCtrl, a3f64 dt)
 					clipCtrl->keyframeTime_sec = overstep;
 					clipCtrl->clipTime_sec = overstep;
 				}
+				//ping-pongs once the end of the clip is reached 
 				else if (clipCtrl->clip->transitionForward->flag == a3clip_reverseFlag)
 				{
 					a3f64 overstep = clipCtrl->clipTime_sec - clipCtrl->clip->duration_sec;
@@ -139,8 +120,10 @@ a3i32 a3clipControllerUpdate(a3_ClipController* clipCtrl, a3f64 dt)
 					clipCtrl->keyframeTime_sec = clipCtrl->keyframe->duration_sec - overstep;
 					clipCtrl->clipTime_sec = clipCtrl->clip->duration_sec - overstep;
 
+					//flag is changed to backward so dt is properly changed 
 					clipCtrl->state = dt > 0 ? BACKWARD : FORWARD;
 				}
+				//playback stops once end of clip is reached
 				else if (clipCtrl->clip->transitionForward->flag == a3clip_stopFlag)
 				{
 					clipCtrl->keyframeIndex = clipCtrl->clip->keyframeCount - 1;
@@ -148,19 +131,14 @@ a3i32 a3clipControllerUpdate(a3_ClipController* clipCtrl, a3f64 dt)
 					clipCtrl->keyframeTime_sec = clipCtrl->keyframe->duration_sec;
 					clipCtrl->clipTime_sec = clipCtrl->clip->duration_sec;
 
+					//flag is changed so dt is 0
 					clipCtrl->state = STOP;
 				}
 			}
+			//time is less than 0 in respect to the clip (backwards)
 			else if (timeStep < 0)
 			{
-				//time is less than 0 in respect to the clip
-
-				//looping
-				//pretty much same as above
-				// ?: offset = clipCtrl->clipTimme_sec
-				//set keyframe index to clipCtrl->clip->keyframeCount(- 1?)
-				//add offset (offset should be negative)
-				//check correct keyframe
+				//loops back to last keyframe after first one
 				if (clipCtrl->clip->transitionReverse->flag == a3clip_playFlag)
 				{
 					a3f64 overstep = clipCtrl->clipTime_sec;
@@ -169,6 +147,7 @@ a3i32 a3clipControllerUpdate(a3_ClipController* clipCtrl, a3f64 dt)
 					clipCtrl->keyframeTime_sec = clipCtrl->keyframe->duration_sec + overstep;
 					clipCtrl->clipTime_sec = clipCtrl->clip->duration_sec + overstep;
 				}
+				//ping-pongs once the beginning of the clip is reached
 				else if (clipCtrl->clip->transitionReverse->flag == a3clip_reverseFlag)
 				{
 					a3f64 overstep = clipCtrl->clipTime_sec;
@@ -177,8 +156,10 @@ a3i32 a3clipControllerUpdate(a3_ClipController* clipCtrl, a3f64 dt)
 					clipCtrl->keyframeTime_sec = -overstep;
 					clipCtrl->clipTime_sec = -overstep;
 
+					//flag is changed to backward so dt is properly changed 
 					clipCtrl->state = dt < 0 ? BACKWARD : FORWARD;
 				}
+				//playback stops once start of clip is reached
 				else if (clipCtrl->clip->transitionReverse->flag == a3clip_stopFlag)
 				{
 					clipCtrl->keyframeIndex = 0;
@@ -186,30 +167,29 @@ a3i32 a3clipControllerUpdate(a3_ClipController* clipCtrl, a3f64 dt)
 					clipCtrl->keyframeTime_sec = 0;
 					clipCtrl->clipTime_sec = 0;
 
+					//flag is changed so dt is 0
 					clipCtrl->state = STOP;
 				}
-				/*while (clipCtrl->keyframeTime_sec >= clipCtrl->keyframe->duration_sec || clipCtrl->keyframeTime_sec < 0)
-				{
-					clipCtrl->keyframeIndex--;
-					clipCtrl->keyframeTime_sec += clipCtrl->keyframe->duration_sec;
-				}*/
-			}// if we need else is dt == 0
+			}
 		}
 		
-		//if keyframe timme exits keyframe bounds
+		//if keyframe time exits keyframe bounds
 		while(clipCtrl->keyframeTime_sec >= clipCtrl->keyframe->duration_sec || clipCtrl->keyframeTime_sec < 0)
 		{
-			//same thing as exiting clip bounds but with keyframe
+			//time is greater than or equal to the keyframe duration (forward)
 			if (timeStep >= 0)
 			{
+				//failsafe to make sure we dont go past the amount of keyframes in the clip
 				if (clipCtrl->keyframeIndex == clipCtrl->clip->keyframeCount - 1)
+				{
 					break;
+				}					
 
 				clipCtrl->keyframeTime_sec -= clipCtrl->keyframe->duration_sec;
 				clipCtrl->keyframeIndex++;
 				clipCtrl->keyframe = &clipCtrl->clipPool->keyframe[clipCtrl->keyframeIndex];
-				//clipCtrl->keyframeIndex = clipCtrl->clip->keyframeCount == clipCtrl->keyframeIndex ? 0 : clipCtrl->keyframeIndex;
 			}
+			//time is less than 0 in respect to the keyframe (backwards)
 			else
 			{
 				clipCtrl->keyframeIndex--;
@@ -217,43 +197,10 @@ a3i32 a3clipControllerUpdate(a3_ClipController* clipCtrl, a3f64 dt)
 				clipCtrl->keyframeTime_sec += clipCtrl->keyframe->duration_sec;
 			}
 		}
-// 
-//3. post-resolution: normalize time/parameters: one time, single line of code applied twice to calculate normalized "keyframe time" and "clip time"
-//	a. normalize keyframe/clip time: relative time / duration
-//	b. 0-1 (not clamped)
 
+		//normalize clip and keyframe time 
 		clipCtrl->clipParam = clipCtrl->clipTime_sec / clipCtrl->clip->duration_sec;
 		clipCtrl->keyframeParam = clipCtrl->keyframeTime_sec / clipCtrl->keyframe->duration_sec;
-//4. add a new instance of the clip controller, new data, and link them to something in the scene to produce a new animation effect
-//	a. required for team of 3
-// 
-//BONUS. Parse the custom clip data file format provided to automate the preparation of clips and keyframe data
-// 
-// 
-//-----------------------------------------------------------------------------
-//			Victor Notes
-//-----------------------------------------------------------------------------
-
-
-		//step 2 check if you're on another keyframe (resolve keyframe)
-		// while realtime is greater than t1 move to the next time
-		//relative time -= duration of the keyframe  &  keyframe moves to the next one
-		//pause
-		//forward
-		//	stop
-		//	step(s) taken
-		//  clip exited
-		//reverse
-		//	stop
-		//	step(s) taken
-		//  clip exited
-
-		//step 3 evaluate where you are in your keyframe
-		//u = (t-t0)/(t1-t0)  ||  this is the value from 0-1 of how far into the clip we are
-		//u: [0, 1)
-
-		// keep stepping condition below, exit is the opposite
-		//while(t>=t1 || t<t0)
 
 //-----------------------------------------------------------------------------
 //****END-TO-DO-PROJECT-1
